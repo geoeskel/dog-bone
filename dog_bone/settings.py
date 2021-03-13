639aa759c215874dd03387dcdbff1a1c1d8da008
geoeskel <geoeskel@gmail.com>
2021-03-13 15:09:51 +0000

    added home app and templates

--- dog_bone/settings.py
+++ dog_bone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account', # handles login/logout/password reset
     'allauth.socialaccount', # handles logging via social media accounts
+    'home', # access to created home app
 
 ]
 
@@ -59,7 +61,10 @@ ROOT_URLCONF = 'dog_bone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
