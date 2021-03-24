4e5a374f23aef1d0b86a21a61ef4ef8abe3337b9
geoeskel <geoeskel@gmail.com>
2021-03-24 21:19:06 +0000

    More product size formatting

--- basket/contexts.py
+++ basket/contexts.py
@@ -14,18 +14,34 @@ def basket_contents(request):
     # Accessing the session's shopping basket
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        # List of bag items dictionary containing item id,
-        # amount of items and the product object (product image, etc.)
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
-
+    for item_id, item_data in basket.items():
+        # Checking if item_data has size
+        # (if it is an integer it will be a quantity only)
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            # List of bag items dictionary containing item id,
+            # amount of items and the product object (product image, etc.)
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            # If it has size, we need to iterate through the dictionary
+            # incrementing item total accordingly
+            # and adding the item to the basket
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                basket_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
     grand_total = delivery + total
 
     context = {
--- basket/views.py
+++ basket/views.py
@@ -21,14 +21,32 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    #  Checking if product has a size in request.POST
+    # if yes, overrite the None with the value from request.POST
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # We create 'basket' variable
     # to store the basket content in the browsing session
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    # If the item is in the basket, increment the quantity.
+    # If the item is not in the basket, add it as dictionary
+    # It allows us adding multiple items with the same id
+    # but different sizes (e.g. Dog Food with 1kg and 5kg)
+    if size:
+        if item_id in list(basket.keys()):
+            if size in basket[item_id]['items_by_size'].keys():
+                basket[item_id]['items_by_size'][size] += quantity
+            else:
+                basket[item_id]['items_by_size'][size] = quantity
+        else:
+            basket[item_id] = {'items_by_size': {size: quantity}}
     else:
-        basket[item_id] = quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
     print(request.session['basket'])
