181dab552ded376d676bdef4a88d013d935eb9a9
geoeskel <geoeskel@gmail.com>
2021-03-27 17:59:05 +0000

    payment core functionality done

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,11 +1,11 @@
 /*  Getting 'public_key' and 'client_secret' from the template using jquery 
     We're slicing first and last character as they will be quotation marks
     JS from: https://stripe.com/docs/payments/accept-a-payment */
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-/* CSS from : https://stripe.com/docs/stripe-js */
+// CSS from : https://stripe.com/docs/stripe-js
 var style = {
     base: {
         color: '#000',
@@ -22,5 +22,71 @@ var style = {
     }
 };
 var card = elements.create('card', {style: style});
-/* Mounting the card to the 'card-element' in checkout.html */
+//  Mounting the card to the 'card-element' in checkout.html
 card.mount('#card-element');
+
+/*  Listener on 'card-elements' for change event checking for errors
+    and displaying them in 'errorDiv' */
+
+    /* Stripe works payment intents.
+1. 	User hits the checkout page
+2. 	The checkout view will call out to stripe and create a payment intent
+	for the current amount of the shopping bag
+3. 	Stripe will create it and attach a secret key for identification
+4. 	Secret key will be returned to us and we'll send it to the template
+	as the 'client_secret' variable.
+5. 	Using the 'client_secret' key we call the 'confirmCardPayment()'
+    method from stripe JS to verify the card number */
+    
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+/*  Handling the submit function (listener)
+1.  Prevent default action (POST)
+2.  Disable card update to prevent multiple submissions
+3.  Execute the 'confirmCardPayment' method
+4.  Call the 'confirmCardPayment' method
+5.  Provide the card to Stripe
+6.  Execute the 'function(result)'
+7.  If 'errorDiv', show it in card div and allow user to resubmit card
+8.  If status is success, submit the form */
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,29 +1,51 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 # Create your views here.
 
-"""
-Getting basket from the session, if nothins in the basket, print error message
-Afterwards, redirect ot products page
-"""
-
-
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    """
+    Getting basket from the session, if nothins in the basket,
+    print error message. Afterwards, redirect ot products page
+    """
+
     basket = request.session.get('basket', {})
     if not basket:
-        messages.error(request, "There's nothing in your bag at the moment")
+        messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
+
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    # Stripe requires the amount to charge as an int
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    # Payemnt intent
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     # Create instance of the order form and render it out
     order_form = OrderForm()
+
+    # Alert message if you forgot to set STRIPE_PUBLIC_KEY
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IZdbqLrqSPomDibbcoOzHrQruzwTEaHTpvwZxaO8VR6A42wE88lEMmKJYjbVlq7NcvJSToEKkPYaMKwAavD6l1o00D7DtqFIA',
-        'client_secret': 'client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
