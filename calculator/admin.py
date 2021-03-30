2b1fe847b2af3cde51a8d43009cc580b251c2542
geoeskel <geoeskel@gmail.com>
2021-03-30 15:13:30 +0000

    added fixtures to calculator app - not leaded yet

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# imports Breed from .models
+from .models import Breeds
 
 # Register your models here.
+
+# registering breed and category breed admin
+admin.site.register(Breeds)
--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+#   Category model (inherit from models.Model)
+class Breeds(models.Model):
+    name = models.CharField(max_length=254)
+    #   friendly name is optional (null and blank = True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    #   string name definition
+    def __str__(self):
+        return self.name
+
+    #   returns friendly name if exists
+    def get_friendly_name(self):
+        return self.friendly_name
