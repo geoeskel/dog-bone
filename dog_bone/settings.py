535e73a2b872c42a3fc680798826fb53a33e158e
geoeskel <geoeskel@gmail.com>
2021-03-31 12:33:20 +0000

    deploying #2

--- dog_bone/settings.py
+++ dog_bone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ssyyd)jyp(w9-fbd!z!_6ncb$snsrr5o$8*tyh@+=@rf&%5awg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dog-bone.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'dog_bone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.sqlite3',
-           'NAME': BASE_DIR / 'db.sqlite3',
-       }
-   }
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
