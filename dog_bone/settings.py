b4ad26702af84e947972cdc5220ba30ad3ea744c
geoeskel <geoeskel@gmail.com>
2021-03-31 13:05:14 +0000

    deploying #4

--- dog_bone/settings.py
+++ dog_bone/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ssyyd)jyp(w9-fbd!z!_6ncb$snsrr5o$8*tyh@+=@rf&%5awg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dog-bone.herokuapp.com', 'localhost']
 
